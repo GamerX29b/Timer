be0db7ded331d5ee364e72f63d7d5bb3738b5419
Sergei Eyrikh <eyrikh@lanit.ru>
2019-04-26 15:30:59 +0600

    Добавил текстовое отображение

--- src/Timer.java
+++ src/Timer.java
@@ -7,23 +7,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Timer extends Application {
-    public static void main(String[] args){
 
-        launch(args);
-
-        TimeThis th = new TimeThis();
-        Date dammit = th.getTime();
-    }
 
         @Override
         public void start(Stage stage) throws Exception {
             String fxmlFile = "/fxml/hell.fxml";
             FXMLLoader loader = new FXMLLoader();
             Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-            stage.setTitle("JavaFX and Maven");
+            stage.setTitle("Timer");
             stage.setScene(new Scene(root));
             stage.show();
+    }
 
+    public static void main(String[] args){
 
+        launch(args);
+
+        TimeThis th = new TimeThis();
+        Date dammit = th.getTime();
     }
 }
--- src/fxml/jfxcontrl.java
+++ src/fxml/jfxcontrl.java
@@ -1,5 +1,43 @@
 package fxml;
 
-public class jfxcontrl {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+import java.awt.*;
+
+public class jfxcontrl implements Initializable {
+
+        @FXML
+    private Button okButton;
+
+        @FXML
+    private Button dataTimeLabel;
+
+
+        @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // TODO (don't really need to do anything here).
+    }
+        public void setDataTimeLabel(ActionEvent event){
+            System.out.println("Hello World");
+
+            Date now = new Date();
+            DateFormat df = new SimpleDateFormat("dd.mm.yyyy");
+
+            String dateTimeString = df.format(now);
+
+            dataTimeLabel.setText(dateTimeString);
+        }
 }
